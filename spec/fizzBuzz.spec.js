a4ee98c6a2a2d6695a4d437b12a6461d3d1033ff
Mljungho <mathias.ljungholm@gmail.com>
2021-10-20 18:17:21 +0200

    add spec for fizzbuzz class

---
+++ spec/fizzBuzz.spec.js
@@ -0,0 +1,20 @@
+const subject = new FizzBuzz
+
+describe("FizzBuzz"), () => {
+
+    it("is expected to return a number if none of the game rules apply"), () => {
+        expected(subject.check(1)).toEqual(1)
+    })
+
+    it("is expected to return 'fizz' if the number is divisable by 3"), () => {
+        expected(subject.check(3)).toEqual("fizz")
+    })
+
+    it("is expected to return 'buzz' if the number is divisable by 5"), () => {
+        expected(subject.check(5)).toEqual("buzz")
+    })
+
+    it("is expected to return 'fizz-buzz' if the number is divisable by 15"), () => {
+        expected(subject.check(15)).toEqual("fizz-buzz")
+    })
+});
